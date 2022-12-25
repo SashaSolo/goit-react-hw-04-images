9cf54d2d5af9b81a33ef0e56b186717dc7e41b96
Sasha.Solo <sasha.solomannik@gmail.com>
2022-12-25 22:50:45 +0200

    Searchbar hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { SearchBar, Form, Button, Input } from './Searchbar.Styled';
 import { FaSistrix } from 'react-icons/fa';
 
-export class Searchbar extends Component {
-  state = {
-    pictureName: '',
+export function Searchbar({ dataForm }) {
+  const [pictureName, setPictureName] = useState('');
+
+  const handleNameChange = event => {
+    setPictureName(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ pictureName: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.pictureName.trim() === '') {
+    if (pictureName.trim() === '') {
       toast.warn('There is no information in search field!');
       return;
     }
-    this.setState({ pictureName: '' });
-    this.props.dataForm(this.state.pictureName);
+    setPictureName('');
+    dataForm(pictureName);
   };
-  render() {
-    return (
-      <SearchBar>
-        <Form onSubmit={this.handleSubmit}>
-          <Button type="submit">
-            <span>
-              <FaSistrix />
-            </span>
-          </Button>
-          <Input
-            type="text"
-            autocomplete="off"
-            autoFocus
-            value={this.state.pictureName}
-            onChange={this.handleNameChange}
-          />
-        </Form>
-      </SearchBar>
-    );
-  }
+
+  return (
+    <SearchBar>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <span>
+            <FaSistrix />
+          </span>
+        </Button>
+        <Input
+          type="text"
+          autocomplete="off"
+          autoFocus
+          value={pictureName}
+          onChange={handleNameChange}
+        />
+      </Form>
+    </SearchBar>
+  );
 }
